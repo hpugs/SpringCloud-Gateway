3e3ef99bbed7beb33d5e9152d8411f05cf0d0bd8
xinge <ai_hpugs@163.com>
2024-11-14 20:20:37 +0800

    first commit'

---
+++ src/test/java/com/hpugs/localgateway/LocalGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpugs.localgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
